91054592a41c1fcd72bbd829f735eb69f55cd1a0
hquesada179 <hquesada179@unab.edu.co>
2026-02-17 14:34:07 -0500

    Grupos de rutas

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index(){
+        return "lISTADO DE PRODUCTOS";
+    }
+    public function create(){
+        return "CREAR UN PRODUCTO DE  PRODUCTOS";
+    }
+    //
+}
