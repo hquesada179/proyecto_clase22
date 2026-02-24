8e255430f42ba1202879ad1807855aeef7ce2b17
hquesada179 <hquesada179@unab.edu.co>
2026-02-24 14:28:07 -0500

    creacion de migraciones

---
+++ database/migrations/2026_02_24_185759_create_categories_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories_products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->decimal('price');
+            $table->timestamps();
+
+            //llave fornea usuario
+    $table->unsignedBigInteger('category_id');
+    $table->foreignId('category_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories_products');
+    }
+};
---
+++ database/migrations/2026_02_24_191206_create_cart_items_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart_items', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            //llave fornea usuario
+    $table->foreignId('user_id')->references('id')->on('users');
+    $table->foreignId('product_id')->references('id')->on('products');
+        });
+    }
+
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart_items');
+    }
+};
