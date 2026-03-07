d8a3338796ee791d8ffa0610056c4998e729cae6
hquesada179 <hquesada179@unab.edu.co>
2026-03-07 18:16:16 -0500

    se agrego 200 productos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(CategorySeeder::class);
 
-        Product::factory(20)->create();
+        Product::factory(200)->create();
     }
 }
